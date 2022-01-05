0482af07c3c7cac71fb93b7bd2abd326c4d11434
Paula Turner <minipaulitat@gmail.com>
2022-01-05 20:29:00 -0300

    segunda entrega

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+export default function NavBar() {
+  return (
+    <>
+      <div>
+        <nav>
+          <p>Lara Barovero</p>
+          <ul>
+            <li>
+              <a href=""> Home</a>
+            </li>
+            <li>
+              <a href=""> Sobre nosotros</a>
+            </li>
+            <li>
+              <a href=""> Tienda</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
+  );
+}
